9a788e0de813b1c62891c8656be9c15ec155e39b
AYadav06 <amity7205@gmail.com>
2025-09-18 00:47:34 +0530

    add .env.example

--- api/src/config/env.ts
+++ api/src/config/env.ts
@@ -5,7 +5,6 @@ export const ENV={
     PORT:process.env.PORT || 3000,
     MONGODB_URL:process.env.MONGODB_URL ||'',
     JWT_SECRETE:process.env.JWT_SECRETE ||'defaultSecret',
-    CLIENT_URL:process.env.CLIENT_URL,
     COOKIE_NAME:"token"
 
 }
